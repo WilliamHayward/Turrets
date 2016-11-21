d96ab3ce27fe26d7e4158f3c204a1117cd4940b1
William Hayward <haywardwilliam1996@gmail.com>
2016-11-22 07:38:54 +1000

    Initial Commit

---
+++ desktop/src/thisisxanderh/turrets/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package thisisxanderh.turrets.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import thisisxanderh.turrets.Turrets;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Turrets(), config);
+	}
+}
